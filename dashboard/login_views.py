4a4fdf6e53ff44f2af5568450585ca08c6f55d13
kentsay <naturen@gmail.com>
2016-09-14 19:03:16 +0200

    modify login check by using redirect and django messages platform

--- dashboard/login_views.py
+++ dashboard/login_views.py
@@ -1,33 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib import messages
 
 def login(request):
     if request.user.is_authenticated:
-        return render(request, 'dashboard/sbadmin/pages/index.html')
+        return redirect('/dashboard/index')
     else:
         return render(request, 'dashboard/sbadmin/pages/login.html')
 
 def logout(request):
     auth_logout(request)
-    return render(request, 'dashboard/sbadmin/pages/login.html')
+    return redirect(login)
 
 def login_check(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
     if user is not None:
+        # Redirect to a success page.
         auth_login(request, user)
         print "success login"
-        return render(request, 'dashboard/sbadmin/pages/index.html')
-        # Redirect to a success page.
+        return redirect('/dashboard/index')
+
     else:
-        context = {
-            'errors': True,
-            'messages': "login fail"
-        }
-        return render(request, 'dashboard/sbadmin/pages/login.html', context)
         # Return an 'invalid login' error message.
+        messages.error(request, 'Username or password didn\'t match. Please try again.')
+        return redirect('login')
