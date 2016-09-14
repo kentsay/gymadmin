a40cb71a62c3105e7915c37b423f2f7d68253717
kentsay <naturen@gmail.com>
2016-09-14 20:35:50 +0200

    add LOGIN_URL in settings

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,7 +11,7 @@ import datetime
 from .members_form import MembersForm
 from .models import Gym
 
-@login_required(login_url='login')
+@login_required
 def index(request):
     gym_list = Gym.objects.all()
     context = {
@@ -20,15 +20,15 @@ def index(request):
     }
     return render(request, 'dashboard/sbadmin/pages/index.html', context)
 
-@login_required(login_url='login')
+@login_required
 def tables(request):
     return render(request, 'dashboard/sbadmin/pages/tables.html')
 
-@login_required(login_url='login')
+@login_required
 def forms(request):
     return render(request, 'dashboard/sbadmin/pages/forms.html')
 
-@login_required(login_url='login')
+@login_required
 def members_add(request):
     if request.method == 'POST':
         form = MembersForm(request.POST)
@@ -45,16 +45,16 @@ def members_add(request):
     context = {'gym_list': enumerate(gym_list, start=1), 'gym_number': len(gym_list), 'form:': form}
     return render(request, 'dashboard/sbadmin/pages/add_members.html', context)
 
-@login_required(login_url='login')
+@login_required
 def members_edit(request):
     return render(request, 'dashboard/sbadmin/pages/edit_members.html')
 
-@login_required(login_url='login')
+@login_required
 def gym(request, gym_id):
     response = "You're looking at gym %s."
     return HttpResponse(response % gym_id)
 
-@login_required(login_url='login')
+@login_required
 def member(request, member_id):
     response = "You're looking at member %s."
     return HttpResponse(response % member_id)
