b77468a5e3ab544c3b07de286f1ddff1d383d4b4
kentsay <naturen@gmail.com>
2016-09-15 12:35:49 +0200

    fix code indent

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-import datetime
-
 from .members_form import MembersForm
 from .models import Gym
+
 
 @login_required
 def index(request):
@@ -17,13 +16,16 @@ def index(request):
     }
     return render(request, 'dashboard/sbadmin/pages/index.html', context)
 
+
 @login_required
 def tables(request):
     return render(request, 'dashboard/sbadmin/pages/tables.html')
 
+
 @login_required
 def forms(request):
     return render(request, 'dashboard/sbadmin/pages/forms.html')
+
 
 @login_required
 def members_add(request):
@@ -42,14 +44,17 @@ def members_add(request):
     context = {'gym_list': enumerate(gym_list, start=1), 'gym_number': len(gym_list), 'form:': form}
     return render(request, 'dashboard/sbadmin/pages/add_members.html', context)
 
+
 @login_required
 def members_edit(request):
     return render(request, 'dashboard/sbadmin/pages/edit_members.html')
+
 
 @login_required
 def gym(request, gym_id):
     response = "You're looking at gym %s."
     return HttpResponse(response % gym_id)
+
 
 @login_required
 def member(request, member_id):
