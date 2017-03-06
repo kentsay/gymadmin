40af1b6d2f6550bb1e0d1a85a20a8536e7fbc88a
kentsay <naturen@gmail.com>
2017-03-06 16:56:12 +0100

    add new models into admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
+import models
 # Register your models here.
-from models import Members, Gym, GymPlan, PaymentPeriod, GymActivity, FightTeam, PaymentRecord
 
-admin.site.register(Members)
-admin.site.register(Gym)
-admin.site.register(GymPlan)
-admin.site.register(PaymentPeriod)
-admin.site.register(GymActivity)
-admin.site.register(FightTeam)
-admin.site.register(PaymentRecord)
+admin.site.register(models.Members)
+admin.site.register(models.Gym)
+admin.site.register(models.GymPlan)
+admin.site.register(models.PaymentPeriod)
+admin.site.register(models.GymActivity)
+admin.site.register(models.FightTeam)
+admin.site.register(models.PaymentRecord)
+admin.site.register(models.GymCheckin)
