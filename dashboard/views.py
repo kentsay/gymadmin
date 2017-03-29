3e216b02b91e0a94a28f9d7eb66b07f421592ed6
kentsay <naturen@gmail.com>
2017-03-29 16:23:58 +0200

    merge code

--- dashboard/views.py
+++ dashboard/views.py
@@ -59,7 +59,18 @@ def members(request):
 @login_required
 def members_detail(request):
     member_list = Members.objects.get(id=request.GET['id'])
+    gym_list = Gym.objects.all()
+    plan = GymPlan.objects.all()
+    payment = PaymentPeriod.objects.all()
+    activity = GymActivity.objects.all()
+    team = FightTeam.objects.all()
     context = {
+        'gym_list': enumerate(gym_list, start=1),
+        'gym_number': len(gym_list),
+        'plan_list': enumerate(plan, start=1),
+        'payment_list': payment,
+        'acitiviy_list': activity,
+        'team_list': team,
         'members': member_list,
     }
     return render(request, 'dashboard/sbadmin/pages/members_detail.html', context)
